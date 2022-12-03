8307c4f6cb2e08b0329c4cce279d1be62eb09bf6
unknown <fahadkabooro.fk@gmail.com>
2022-12-03 23:04:27 +0500

    class 11 updated

---
+++ js class 11/main.js
@@ -0,0 +1,59 @@
+/*
+(  TODAYS TOPICS  ) 
+input , radio , checkbox, slect
+.value => property => data
+byid => unique
+class
+tagname
+event.prventDefault
+onchange event
+
+
+*/
+
+
+var email = document.getElementById("email")
+var password = document.getElementById("password")
+var emailtxt = document.getElementById("emailtxt")
+var passwordtxt = document.getElementById("passwordtxt")
+var submittxt = document.getElementById("submittxt")
+var selectcountry = documen.getElementById()
+function getdata(){
+    event.preventDefault()//form
+    if(email.value==""){
+        emailtxt.className="show red"
+    }
+  else  if(password.value==""){
+        emailtxt.className="hide red"
+        passwordtxt.className="show red"
+    }
+    else{
+        emailtxt.className="hide red"
+        passwordtxt.className="hide red"
+        submittxt.className="show"
+        console.log(email.value)
+        console.log(emailtxt.className)
+
+        setTimeout(()=>{
+            email.value=""
+            password.value=""
+            submittxt.value="hide"
+        },2000)
+    }
+function setemail(){
+    if(email.value!=""){
+        emailtxt.className="hide"
+        if(password.value!=""){
+            passwordtxt.className="hide"
+        }
+        else{
+            passwordtxt.className="show"
+        }
+
+    }
+}
+
+
+function setcountry(){
+    console.log(selectcountry.value)
+}
